5a5be9276b44e6f9a2bd06d9b14427fc6167b63f
Chaitanya Hazarika <hiranya935@gmail>
2024-04-06 17:28:13 +0530

    first commit

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,85 @@
+import 'package:expenses_tracker/widgets/chart/chart.dart';
+import 'package:expenses_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expenses_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+import 'package:expenses_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpense(
+        onAddExpense: _addexpense,
+      ),
+    );
+  }
+
+  void _addexpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense); 
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: Text('Expense Deleted'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registeredExpenses.insert(expenseIndex, expense);
+              });
+            }),
+      ),
+    );
+  }
+
+  @override
+  Widget build(context) {
+    Widget mainContent =
+        Center(child: Text('No expenses found start adding some!'));
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(
+              Icons.add,
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [Chart(expenses: _registeredExpenses,),
+          Expanded(
+            child: mainContent,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,30 @@
+import 'package:expenses_tracker/widgets/expenses_list/expenses_item.dart';
+import 'package:flutter/material.dart';
+import 'package:expenses_tracker/models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
+
+  final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
+  @override
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          margin: Theme.of(context).cardTheme.margin,
+        ),
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpensesItem(
+          expenses[index],
+        ),
+      ),
+    );
+  }
+}
